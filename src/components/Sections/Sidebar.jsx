93a914fbc19bb51e521c5e47936c6ceb34339ac3
vi le <vile06062001@gmail.com>
2022-03-28 16:05:33 +0700

    fix ui

--- src/components/Sections/Sidebar.jsx
+++ src/components/Sections/Sidebar.jsx
@@ -1,68 +1,66 @@
-import { useDispatch, useSelector } from 'react-redux';
-import Avatar from '@mui/material/Avatar';
-import { Link, useNavigate } from 'react-router-dom';
-import { useSnackbar } from 'notistack';
-import { logoutUser } from '../../actions/userAction';
-import React from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import Avatar from "@mui/material/Avatar";
+import { Link, useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
+import { logoutUser } from "../../actions/userAction";
+import React from "react";
 const Sidebar = ({ activeTab }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const { enqueueSnackbar } = useSnackbar();
+  const { user } = useSelector((state) => state.user);
+  console.log(user + "sidebar account");
+  const handleLogout = () => {
+    dispatch(logoutUser());
+    enqueueSnackbar("Logout Successfully", { variant: "success" });
+    navigate("/login");
+  };
 
-    const { user } = useSelector(state => state.user);
-    console.log(user + "sidebar account")
-    const handleLogout = () => {
-        dispatch(logoutUser());
-        enqueueSnackbar("Logout Successfully", { variant: "success" });
-        navigate("/login");
-    }
-
-    return (
-        <>
-            <div className="wrapper">
-                {/* Sidebar Holder */}
-                <nav id="sidebar" >
-                    <div className="sidebar-header flex">
-
-                        <h3>{user.name}</h3>
-                    </div>
-                    <ul className="list-unstyled components">
-                        <li className=''>
-                            <Link to="/account" className={`${activeTab === "profile"} `} >
-                                <i className="fa fa-home" />
-                                Tài khoản
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/shop" className={`${activeTab === "shop"} `} >
-                                <i className="fa fa-home" />
-                                Bán hàng
-                            </Link>
-                        </li>
-                        <li>
-                            <a href="/shop/product">
-                                <i className="fa fa-link" />
-                                Sản phẩm
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="fa fa-paperclip" />
-                                Đơn đặt hàng
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i className="fa fa-send" />
-                                Contact
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="wrapper">
+        {/* Sidebar Holder */}
+        <nav id="sidebar">
+          <div className="sidebar-header flex">
+            <h3>{user.name}</h3>
+          </div>
+          <ul className="list-unstyled components">
+            <li className="">
+              <Link to="/account" className={`${activeTab === "profile"} `}>
+                <i className="fa fa-home" />
+                Tài khoản
+              </Link>
+            </li>
+            <li>
+              <Link to="/shop" className={`${activeTab === "shop"} `}>
+                <i className="fa fa-users" />
+                Bán hàng
+              </Link>
+            </li>
+            <li>
+              <a href="/shop/product">
+                <i className="fa fa-link" />
+                Sản phẩm
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-paperclip" />
+                Đơn đặt hàng
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-id-card" />
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
 };
 
 export default Sidebar;
